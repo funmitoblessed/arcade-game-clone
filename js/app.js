1d69fdf364bfca087fdc0341b6c6ef5265ac601f
Oluwafunmito Blessed <funmito.odefemi@outlook.com>
2018-11-22 18:36:44 +0100

    update game to allow player selection

--- js/app.js
+++ js/app.js
@@ -5,101 +5,62 @@ let user = prompt("Before we begin, please tell me your name");
 let scoreHolder = document.getElementById('score');
 let gameScore = scoreHolder.innerHTML = 0;
 
-// Enemies our player must avoid
-let Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// initialize menu
+let menu = new Menu();
 
-    // enemy position
-    this.x = x;
-    this.y = y;
-    // enemy speed
-    this.speed = speed;
+// generate some random numbers
+let randOne = Math.floor(Math.random() * 200);
+let randTwo = Math.floor(Math.random() * 200);
+let randThree = Math.floor(Math.random() * 200);
+let randFour = Math.floor(Math.random() * 200);
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// create new instances of enemies with random x-position and speed and fixed y
+let allEnemies = [new Enemy(randOne, 230, randThree),
+    new Enemy(randTwo, 140, randOne),
+    new Enemy(randThree, 310, randFour),
+    new Enemy(randFour, 60, randTwo),
+]
 
-};
+// Instantiate player with default position
+// Place the player object in a variable called player
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x < 500) {
-        this.x += this.speed * dt + Math.floor(Math.random() * 5);
+let player = new Player();
+gameTimer();
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    let allowedKeys = {
+        37: 'left', // ←
+        38: 'up', // ↑
+        39: 'right', // →
+        40: 'down', // ↓
+        65: 'left', // A
+        68: 'right', // D
+        83: 'down', // S
+        87: 'up', // W
+        // Character selection
+        49: 'boy', // 1
+        50: 'cat-girl', // 2
+        51: 'horn-girl', // 3
+        52: 'pink-girl', // 4
+        53: 'princess-girl' // 5
+    };
+
+    if (menu.on) {
+        menu.handleInput(allowedKeys[e.keyCode]);
     } else {
-        this.x = -100;
+        player.handleInput(allowedKeys[e.keyCode]);
+        player.score(allowedKeys[e.keyCode]);
     }
 
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-let Player = function(x, y) {
-    // image sprite for player
-    this.sprite = 'images/char-pink-girl.png';
-
-    // player position
-    this.x = x;
-    this.y = y;
-
-};
-
-// update method for the Player class
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    for (const enemy of allEnemies) {
-        if ((this.x < enemy.x + 80) &&
-            (this.x + 80 > enemy.x) &&
-            (this.y < enemy.y + 60) &&
-            (60 + this.y > enemy.y)) {
-            resetPlayer();
-        }
-    }
-
-};
-
-// render method for the Player class
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+});
 
 
-// handle input method for the Player class
-Player.prototype.handleInput = function(key) {
-    if ((key === 'left') && (this.x !== 0)) {
-        this.x -= 100;
-    } else if ((key === 'right') && (this.x !== 400)) {
-        this.x += 100;
-    } else if ((key === 'up') && (this.y !== 10)) {
-        this.y -= 70;
-    } else if ((key === 'down') && (this.y !== 410)) {
-        this.y += 70;
-    }
-};
-
-// player scoring method. Adds 1 to the score once the player reaches the river
-Player.prototype.score = function(key) {
-    if (this.y <= 0) {
-        if ((key === 'up') && (this.y === -10)) {
-            this.x = 200;
-            this.y = 410;
-        }
-        gameScore++;
-        scoreHolder.innerHTML = gameScore;
-    }
-};
 
 // resets player position on collision and reduces game score
 function resetPlayer() {
@@ -112,42 +73,6 @@ function resetPlayer() {
     }
     scoreHolder.innerHTML = gameScore;
 };
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-// Instantiate player with default position
-let player = new Player(200, 410);
-
-// generate some random numbers
-let randOne = Math.floor(Math.random() * 200);
-let randTwo = Math.floor(Math.random() * 200);
-let randThree = Math.floor(Math.random() * 200);
-let randFour = Math.floor(Math.random() * 200);
-
-// create new instances of enemies with random x-position and speed and fixed y
-let allEnemies = [new Enemy(randOne, 230, randThree),
-    new Enemy(randTwo, 140, randOne),
-    new Enemy(randThree, 310, randFour),
-    new Enemy(randFour, 60, randTwo),
-]
-
-gameTimer();
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    let allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.which]);
-    player.score(allowedKeys[e.which]);
-});
 
 
 // timer function
@@ -172,7 +97,7 @@ function gameTimer() {
         }
         s = addZero(s);
         timer.innerHTML = h + ":" + m + ":" + s;
-        if (m == 01 && s == 00) {
+        if (m == 01 && s == 30) {
             alert(`Congratulations ${user}! You won this game with ${gameScore} points! Press OK to restart the game`);
             clearInterval(t);
             timer.innerHTML = '0:00:00';
@@ -182,15 +107,8 @@ function gameTimer() {
             gameScore = 0;
             scoreHolder.innerHTML = gameScore;
             gameTimer();
-            console.log('Time Up!');
         }
     }, 1000);
-
-    // function endGame() {
-
-    // };
-
-    // endGame();
 };
 
 // add zero in front of numbers < 10
@@ -198,6 +116,3 @@ function addZero(i) {
     if (i < 10) { i = "0" + i };
     return i;
 };
-
-
-console.log(allEnemies[0], allEnemies[1], allEnemies[2], allEnemies[3], player);
